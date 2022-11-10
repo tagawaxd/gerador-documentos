3c09acec56e0bd5a95208819d058e26f9c8fdc76
INDRA\vtagawa <vtagawa@minsait.com>
2022-11-10 12:27:56 -0300

    inicio projeto

---
+++ src/main/java/br/com/indra/geradordedoc/GeradorDocumentoIndraApplication.java
@@ -0,0 +1,13 @@
+package br.com.indra.geradordedoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorDocumentoIndraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorDocumentoIndraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/indra/geradordedoc/GeradorDocumentoIndraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.indra.geradordedoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeradorDocumentoIndraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
